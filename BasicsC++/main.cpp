61246ae9d4ad3d65954dcfa64c0e4874c75b7556
Kirill12a <kirilldrozd12@gmail.com>
2021-09-08 23:22:33 +0300

    add

--- BasicsC++/main.cpp
+++ BasicsC++/main.cpp
@@ -1,14 +1,24 @@
-//
-//  main.cpp
-//  BasicsC++
-//
-//  Created by Kirill Drozdov on 07.09.2021.
-//
 
 #include <iostream>
+#include <string>
+using namespace::std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+
+
+
+int main(){
+    int a[] = {1,4,6,9,2,3,52,74};
+    for (int i = 0; i<8; i++){
+        for (int j = 0; j<8-1; j++){
+            if (a[j]>a[j+1]) {
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+    cout << endl;
+    for (int i = 0; i<8; i++){
+        cout << a[i] << " ";
+    }
 }
+
